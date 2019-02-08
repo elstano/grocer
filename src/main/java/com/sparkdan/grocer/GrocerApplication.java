950f2a494cb58cdad8991b892a2437d1361849be
nekrylov <nekrylov@netcracker.com>
2019-02-08 14:43:50 +0300

    initial spring application

---
+++ src/main/java/com/sparkdan/grocer/GrocerApplication.java
@@ -0,0 +1,14 @@
+package com.sparkdan.grocer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrocerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrocerApplication.class, args);
+	}
+
+}
+
