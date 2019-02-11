978c4daea029c26ac4ddadf4c9a46a5fb3242790
nekrylov <nekrylov@netcracker.com>
2019-02-11 19:08:53 +0300

    initialize bootstrap, jquery, jquery bootgrid, spring security

--- src/main/java/com/sparkdan/grocer/WebSecurityConfig.java
+++ src/main/java/com/sparkdan/grocer/WebSecurityConfig.java
@@ -17,7 +17,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
+                .antMatchers("/", "/jquery-3.3.1.min.js", "/bootstrap/**", "/bootgrid/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
