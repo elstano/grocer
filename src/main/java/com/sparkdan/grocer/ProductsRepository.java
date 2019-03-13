4b9af6b5360160860eee04b5b4d26c26d700cbe2
nekrylov <nekrylov@netcracker.com>
2019-03-13 22:01:02 +0300

    switch to jsGrid

--- src/main/java/com/sparkdan/grocer/ProductsRepository.java
+++ src/main/java/com/sparkdan/grocer/ProductsRepository.java
@@ -4,9 +4,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource(collectionResourceRel = "products", path = "products")
 public interface ProductsRepository extends PagingAndSortingRepository<Product, String> {
 //    @Query("select id from product offset :startWith rows fetch first :rowCount rows only order by name asc")
 //    public List<Product> listRegisteredProducts(@Param("startWith") int startWith, @Param("rowCount") int rsowCount);
--- src/main/resources/static/grocer/grocer.js
+++ src/main/resources/static/grocer/grocer.js
@@ -1,34 +1,60 @@
-var grid = $("#grid-data").bootgrid({
-    ajax: true,
-    post: function ()
-    {
-        return {
-            id: "b0df282a-0d67-40e5-8558-c9e93b7befed-test-id"
-        };
-    },
-    url: "/listRegisteredProducts",
-    formatters: {
-        "commands": function(column, row)
-        {
-            return "<button type=\"button\" class=\"btn btn-xs btn-default command-edit\" data-row-id=\"" + row.id + "\"><span class=\"fa fa-pencil\"></span></button> " +
-                "<button type=\"button\" class=\"btn btn-xs btn-default command-delete\" data-row-id=\"" + row.id + "\"><span class=\"fa fa-trash-o\"></span></button>";
-        },
-        "categories" : function (column, row) {
-            let result = "";
-            for(let cat of row.categories) {
-                result += cat.name + ", "
-            }
-            return result;
+var clients = [
+    { "Name": "Otto Clay", "Age": 25, "Country": 1, "Address": "Ap #897-1459 Quam Avenue", "Married": false },
+    { "Name": "Connor Johnston", "Age": 45, "Country": 2, "Address": "Ap #370-4647 Dis Av.", "Married": true },
+    { "Name": "Lacey Hess", "Age": 29, "Country": 3, "Address": "Ap #365-8835 Integer St.", "Married": false },
+    { "Name": "Timothy Henson", "Age": 56, "Country": 1, "Address": "911-5143 Luctus Ave", "Married": true },
+    { "Name": "Ramona Benton", "Age": 32, "Country": 3, "Address": "Ap #614-689 Vehicula Street", "Married": false }
+];
+
+var countries = [
+    { Name: "", Id: 0 },
+    { Name: "United States", Id: 1 },
+    { Name: "Canada", Id: 2 },
+    { Name: "United Kingdom", Id: 3 }
+];
+
+$("#jsGrid").jsGrid({
+    width: "100%",
+    height: "400px",
+
+    inserting: true,
+    editing: true,
+    sorting: true,
+    paging: true,
+    pageSize: 10,
+    autoload: true,
+    pageLoading: true,
+
+    controller: {
+        loadData: function (filter) {
+            var d = $.Deferred();
+            $.ajax({
+                url: "/products",
+                data: {
+                    page: filter.pageIndex - 1,
+                    size: filter.pageSize
+                },
+                dataType: "json",
+                success: function (response) {
+                    d.resolve({
+                        data: response._embedded.products,
+                        itemsCount: response.page.totalElements
+                    });
+                },
+                error: function () {
+                    d.resolve();
+                }
+            });
+            return d.promise();
         }
-    }
-}).on("loaded.rs.jquery.bootgrid", function()
-{
-    /* Executes after data is loaded and rendered */
-    grid.find(".command-edit").on("click", function(e)
-    {
-        alert("You pressed edit on row: " + $(this).data("row-id"));
-    }).end().find(".command-delete").on("click", function(e)
-    {
-        alert("You pressed delete on row: " + $(this).data("row-id"));
-    });
+    },
+
+    fields: [
+        // { name: "id", type: "text", width: 150, validate: "required" },
+        { name: "name", type: "text", width: 150 },
+        { name: "categories", type: "text", width: 50 },
+        { name: "unit", type: "text", width: 200, validate: "required"  },
+        { name: "increment", type: "text", validate: "required" },
+        { type: "control" }
+    ]
 });
